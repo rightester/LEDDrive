d4cf6ebbc753954288db64255273dadd915d7299
叫我最右君 <33158925+rightester@users.noreply.github.com>
2024-02-16 04:26:02 +0800

    更新font.h，修改了结构体中宽、高项的顺序
    规范顺序，统一宽前高后

--- library/stm32_hal_ch1116/stm32_hal_ch1116/Core/Inc/font.h
+++ library/stm32_hal_ch1116/stm32_hal_ch1116/Core/Inc/font.h
@@ -3,8 +3,8 @@
 #include "stdint.h"
 #include "string.h"
 typedef struct ASCIIFont {
-  uint8_t h;
   uint8_t w;
+  uint8_t h;
   uint8_t *chars;
 } ASCIIFont;
 
@@ -19,8 +19,8 @@ extern const ASCIIFont afont24x12;
  * @note 字库数据可以使用波特律动LED取模助手生成(https://led.baud-dance.com)
  */
 typedef struct Font {
-  uint8_t h;              // 字高度
   uint8_t w;              // 字宽度
+  uint8_t h;              // 字高度
   const uint8_t *chars;   // 字库 字库前4字节存储utf8编码 剩余字节存储字模数据
   uint8_t len;            // 字库长度 超过256则请改为uint16_t
   const ASCIIFont *ascii; // 缺省ASCII字体 当字库中没有对应字符且需要显示ASCII字符时使用
