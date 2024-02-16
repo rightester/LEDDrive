e131360b827225c86f319aa89821a4f2aa3cdf09
叫我最右君 <33158925+rightester@users.noreply.github.com>
2024-02-16 19:58:40 +0800

    Update font.h
    激进的修改，统一了所有字体变量的名称为“宽x高”，所有结构体的字段为先宽后高

--- library/stm32_hal_ch1116/stm32_hal_ch1116/Core/Inc/font.h
+++ library/stm32_hal_ch1116/stm32_hal_ch1116/Core/Inc/font.h
@@ -1,17 +1,18 @@
 #ifndef __FONT_H
 #define __FONT_H
-#include "stdint.h"
-#include "string.h"
+
+#include <stdint.h>
+
 typedef struct ASCIIFont {
   uint8_t w;
   uint8_t h;
   uint8_t *chars;
 } ASCIIFont;
 
-extern const ASCIIFont afont8x6;
-extern const ASCIIFont afont12x6;
-extern const ASCIIFont afont16x8;
-extern const ASCIIFont afont24x12;
+extern const ASCIIFont afont6x8;
+extern const ASCIIFont afont6x12;
+extern const ASCIIFont afont8x16;
+extern const ASCIIFont afont12x24;
 
 /**
  * @brief 字体结构体
